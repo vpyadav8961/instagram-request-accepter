067c1ac963c8c04b9f996f1c55a2d5eb0e614203
vpyadav8961 <vpyadav8961@gmail.com>
2020-02-03 23:07:06 +0530

    Added helper function for generating the Http request

--- app/Support/helpers.js
+++ app/Support/helpers.js
@@ -12,4 +12,46 @@ Helpers.isJSON = (data)=>{
     }
 }
 
+Helpers.generateHttpRequest = (data)=>{
+    console.log('inside create Http request');
+    let device = (this.isJSON(data.device)) ? JSON.parse(data.device): data.device;
+    console.log('Device :: ', device, typeof device);
+
+    var postOption = {
+        url: data.url,
+        proxy: (!_.isUndefined(data.proxy) ? data.proxy: ''),
+        headers: {
+            'X-Pigeon-Session-Id': device.pSession,
+            'X-Pigeon-Rawclienttime': Math.floor(Date.now() / 1000),
+            'X-IG-Connection-Speed': '-1kbps',
+            'X-IG-Bandwidth-Speed-KBPS': _.random(1000,9999)+'kbps',
+            'X-IG-Bandwidth-TotalBytes-B': 0,
+            'X-IG-Bandwidth-TotalTime-MS': 0,            
+            'X-IG-Connection-Type': 'WIFI',
+            'X-IG-Capabilities': '3brTvwE=',
+            'X-IG-App-ID': 567067343352427,
+            'Accept-Language': 'en-US',            
+            'X-FB-HTTP-Engine': 'Liger',
+            'X-Bloks-Version-Id': '0a3ae4c88248863609c67e278f34af44673cff300bc76add965a9fb036bd3ca3',
+            'X-Bloks-Is-Layout-RTL': false,
+            'X-IG-Device-Locale': 'en_US',
+            'X-IG-Device-ID': device.guid,
+            'X-IG-Android-ID': device.androidDeviceId,        
+            'User-Agent': device.user_agent,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Host: 'i.instagram.com',
+           
+        }
+    };
+
+    postOption['headers']['Cookie'] = data.session;
+    if(!_.isUndefined(data.formData)){
+        postOption['form'] = data.formData
+    }
+
+    return postOption;
+}
+
+
+
 module.exports = Helpers;
